9feb0737a8c97f0a36a39062579f77ac5b5f7c37
robink <odisseus@gmail.com>
2010-06-01 17:03:39 +0200

    Fixed issue #5 : jquery.datefield does not appear to work in ajaxformloop

--- src/main/resources/org/got5/tapestry5/jquery/components/datefield.js
+++ src/main/resources/org/got5/tapestry5/jquery/components/datefield.js
@@ -2,12 +2,22 @@
     /** Container of functions that may be invoked by the Tapestry.init() function. */
     $.extend(Tapestry.Initializer, {
         dateField: function(specs) {
+			if (specs.localization && specs.localization.firstDay) {
+				$.tapestry.dateField.firstDay = specs.localization.firstDay;
+			}
+			
             $("#" + specs.field).datepicker({
-                firstDay: specs.localization.firstDay,
+                firstDay: $.tapestry.dateField.firstDay,
                 gotoCurrent: true
             });
         }
     });
+	
+	$.extend($.tapestry, {
+		dateField : {
+			firstDay: 0
+		}
+	});
 })(jQuery);
 
 
