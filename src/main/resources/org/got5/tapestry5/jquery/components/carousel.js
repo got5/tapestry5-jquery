58f7d4b6f263ad30d957460b3bc4802726e91f3f
awillemant <a148744@.fr01.awl.atosorigin.net>
2011-05-03 17:45:45 +0200

    improvement of the carousel test page

--- src/main/resources/org/got5/tapestry5/jquery/components/carousel.js
+++ src/main/resources/org/got5/tapestry5/jquery/components/carousel.js
@@ -7,4 +7,33 @@ $.extend(Tapestry.Initializer, {
     }
 });
 
+
+
+
+
+
 }) ( jQuery );
+
+var loadCarousel = function(url){
+	function mycarousel_itemLoadCallback(carousel, state){
+        if (state != 'init') {
+			return;
+		}
+		jQuery.get(url, function(data) {
+        	mycarousel_itemAddCallback(carousel, carousel.first, carousel.last, data);
+   		});
+	};
+	return mycarousel_itemLoadCallback;
+}
+
+
+function mycarousel_itemAddCallback(carousel, first, last, data){
+   	for (i = 0; i < data.length; i++) {
+        carousel.add(i+1, mycarousel_getItemHTML(data[i]));
+    }
+    carousel.size(data.length);
+};
+
+function mycarousel_getItemHTML(url){
+    return '<img src="' + url + '" width="75" height="75" alt="" />';
+};
--- src/test/java/org/got5/tapestry5/jquery/test/pages/CarouselPage.java
+++ src/test/java/org/got5/tapestry5/jquery/test/pages/CarouselPage.java
@@ -17,9 +17,15 @@
 package org.got5.tapestry5.jquery.test.pages;
 
 import org.apache.tapestry5.Block;
+import org.apache.tapestry5.ComponentResources;
+import org.apache.tapestry5.annotations.AfterRender;
 import org.apache.tapestry5.annotations.OnEvent;
 import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.json.JSONArray;
+import org.apache.tapestry5.json.JSONLiteral;
 import org.apache.tapestry5.json.JSONObject;
+import org.apache.tapestry5.services.javascript.InitializationPriority;
+import org.apache.tapestry5.services.javascript.JavaScriptSupport;
 
 
 public class CarouselPage{
@@ -27,14 +33,49 @@ public class CarouselPage{
 	@Inject
 	private Block flowerBlock;
 	
-	public JSONObject getParams(){
+	@Inject
+	private JavaScriptSupport javascriptSupport;
+	
+	@Inject
+	private ComponentResources resources;
+	
+	public JSONObject getAjaxParams(){
+		JSONObject retour = new JSONObject();
+		retour.put("wrap", "circular");
+		String url = resources.createEventLink("ajaxEvent").toString();
+		retour.put("itemLoadCallback", new JSONLiteral("loadCarousel('"+url+"')"));
+		return retour;
+	}
+	
+	public JSONObject getComplexParams(){
 		JSONObject retour = new JSONObject();
 		retour.put("wrap", "circular");
 		return retour;
 	}
 	
+	@AfterRender
+	public void finish(){
+		//javascriptSupport.addScript(InitializationPriority.LATE, "loadCarousel('%s')", );
+	}
+	
 	public Object getTest(){
 		return "hello";
+	}
+	
+	@OnEvent("ajaxEvent")
+	public Object ajaxtTest(){
+		JSONArray retour = new JSONArray();
+		retour.put("http://static.flickr.com/57/199481087_33ae73a8de_s.jpg");
+		retour.put("http://static.flickr.com/75/199481072_b4a0d09597_s.jpg");
+		retour.put("http://static.flickr.com/77/199481108_4359e6b971_s.jpg");
+		retour.put("http://static.flickr.com/58/199481143_3c148d9dd3_s.jpg");
+		retour.put("http://static.flickr.com/72/199481203_ad4cdcf109_s.jpg");
+		retour.put("http://static.flickr.com/58/199481218_264ce20da0_s.jpg");
+		retour.put("http://static.flickr.com/69/199481255_fdfe885f87_s.jpg");
+		retour.put("http://static.flickr.com/60/199480111_87d4cb3e38_s.jpg");
+		retour.put("http://static.flickr.com/70/229228324_08223b70fa_s.jpg"); 
+		
+		return retour;
 	}
 	
 	@OnEvent("eventTest")
