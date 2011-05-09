872d1b5401f37cfde31dc4e064392c3b81167e75
Clement <clement.uster@gmail.com>
2011-05-09 13:31:13 +0200

    correcting bug w/ jquery autocomplete

--- src/main/resources/org/got5/tapestry5/jquery/mixins/autocomplete.js
+++ src/main/resources/org/got5/tapestry5/jquery/mixins/autocomplete.js
@@ -5,21 +5,21 @@
             var conf = {
                 source: function(request, response){
                     var ajaxRequest = {
+                    	url:specs.url,
                         success: function(data){
                             response(eval(data));
                         }, 
                         data:specs.paramName+"="+request.term, 
                         type:"POST"
-                    }
-                    $.ajax(specs.url, ajaxRequest);
+                    };
+                    $.ajax(ajaxRequest);
                 }
-            }
-            
+            };
             if (specs.delay) 
                 conf.delay = specs.delay;
             
             if (specs.minLength) 
-                conf.minLength = specs.minLength
+                conf.minLength = specs.minLength;
             
             $("#" + specs.id).autocomplete(conf);
         }
