6bf97e8671b8d73e18e48afa17327b1065d3fe64
Clement <clement.uster@gmail.com>
2011-06-22 18:18:44 +0200

    adding client validation (for bean validator project) + regexp support

--- src/main/resources/org/got5/tapestry5/jquery/validation.js
+++ src/main/resources/org/got5/tapestry5/jquery/validation.js
@@ -4,13 +4,11 @@
  */
 $.extend(Tapestry.Initializer, {
     validate: function(/*field, */specs) {    	
-        
         $.each(specs, function(i, ruleSpecs) {
             var field = i;
             $('#' + field).closest('form').validate({
                 errorClass: "t-error"
             });
-            
             $.each(ruleSpecs, function(j, ruleSpec) {
                 var jsRule = $.tapestry.validate.toJSRule(ruleSpec[0]);
                 var ruleAsString = '{"' + jsRule + '" :' + ((ruleSpec[2] === undefined) ? 'true' : '"' + ruleSpec[2] + '"') + ', "messages": {"' + jsRule + '":"' + ruleSpec[1] + '"}}';
@@ -25,6 +23,32 @@ $.extend(Tapestry.Initializer, {
 /**
  * Tapestry static functions
  */
+
+var Tapestry.Validator={};
+$.validator.addMethod("regexp",function( value, element, param ) {
+	var r = new RegExp(param);
+    return this.optional(element) || r.test(value);
+},"The text you entered doesn't match the regular expression");
+
+$.validator.addMethod("isnull",function( value, element ) {
+    return this.optional(element) || value == null;
+},"The value is not null");
+
+$.validator.addMethod("size",function( value, element, param ) {
+	
+	if (Object.isString(value))
+		return this.optional(element) || value.length > param.min || value.length < param.max;
+	else if (Object.isArray(value))
+	{	
+		if(element.tagName == "SELECT")
+		{
+			var selectedOptions = Element.childElements(this).size();
+			return this.optional(element) || selectedOptions > param.min || selectedOptions < param.max;
+		}
+	}
+    return this.optional(element);
+},"The text you entered doesn't have the right size");
+
 $.extend($.tapestry, {
     validate: {
         /**
@@ -35,7 +59,11 @@ $.extend($.tapestry, {
         },
         
         rules: {
-            numericformat: "number"
+            numericformat: "number", 
+            pattern:"regexp",
+            notnull:"required",
+            maxnumber:"max", 
+            minnumber:"min"
         }
     }
 });
