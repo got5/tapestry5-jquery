0b0958a391ae9ddc2ccf0d2e84fd3d304e3a828c
Clement <clement.uster@gmail.com>
2011-07-04 16:39:55 +0200

    issue #52 => AJAX requests coming from tabs are canceled thanks to the ajaxOptions.beforeSend method (returning false in this method cancels the ajax request) but, ajaxOptions.beforeSend can still be overriden if needed (from "params" parameter of the Tabs component)

--- src/main/resources/org/got5/tapestry5/jquery/assets/components/tabs/tabs.js
+++ src/main/resources/org/got5/tapestry5/jquery/assets/components/tabs/tabs.js
@@ -1,7 +1,16 @@
 (function( $ ) {
 	$.extend(Tapestry.Initializer, {
 	    tabs: function(specs) {
-	        $("#" + specs.id).tabs(specs.params);
+			var p = specs.params;
+			console.log(p);
+			if(!p.ajaxOptions)
+				p.ajaxOptions={};
+			if(!p.ajaxOptions.beforeSend)
+				$.extend(p.ajaxOptions, {beforeSend:function(){
+					//returning false in beforeSend function cancels the AJAX request, see issue #52
+					return false;
+				}});
+			$("#" + specs.id).tabs(p);
 	    }
 	});
 }) ( jQuery );
