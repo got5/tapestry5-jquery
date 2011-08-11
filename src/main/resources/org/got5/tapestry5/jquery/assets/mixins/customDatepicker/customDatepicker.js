7028b6b9b7a6b01f9d91b4d87f75f9c1aae7db95
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2011-08-11 17:11:59 +0200

    Change the CustomZone mixin : use the jQuery alias and the option parameter of the widget

--- src/main/resources/org/got5/tapestry5/jquery/assets/mixins/customDatepicker/customDatepicker.js
+++
@@ -1,9 +0,0 @@
-(function($){
-    
-	/** Container of functions that may be invoked by the Tapestry.init() function. */
-    $.extend(Tapestry.Initializer, {
-    	customDatepicker: function(specs){
-    		$("#" + specs.id).datepicker('option',specs.params);
-        }
-    });
-})(jQuery);
