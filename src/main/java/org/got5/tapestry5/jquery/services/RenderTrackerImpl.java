ff128d1d5e4bc73236cfb2f1ba7625465cb31c82
Barry Books <bfb@trsvax.com>
2011-08-12 14:14:53 -0500

    Convert to stack

--- src/main/java/org/got5/tapestry5/jquery/services/RenderTrackerImpl.java
+++ src/main/java/org/got5/tapestry5/jquery/services/RenderTrackerImpl.java
@@ -2,6 +2,7 @@ package org.got5.tapestry5.jquery.services;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Stack;
 
 import org.apache.tapestry5.ioc.ScopeConstants;
 import org.apache.tapestry5.ioc.annotations.Scope;
@@ -9,15 +10,19 @@ import org.apache.tapestry5.runtime.Component;
 
 @Scope(ScopeConstants.PERTHREAD)
 public class RenderTrackerImpl implements RenderTracker {
-	private Component component;
+	private Stack<Component> components = new Stack<Component>();
 	private Map<String, Boolean> ids = new HashMap<String, Boolean>();
 
-	public void setRendering(Component component) {
-		this.component = component;
+	public void push(Component component) {
+		components.push(component);
+	}
+	
+	public void pop() {
+		components.pop();
 	}
 
 	public Component getRendering() {
-		return component;
+		return components.peek();
 	}
 
 	public Map<String, Boolean> getIdMap() {
