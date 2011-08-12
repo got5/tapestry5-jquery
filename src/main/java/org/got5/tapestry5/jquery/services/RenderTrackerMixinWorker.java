3c4a0728a0ea79d3d00a3c88f994c5f023d221ad
Barry Books <bfb@trsvax.com>
2011-08-12 17:03:20 -0500

    removed unneeded code

--- src/main/java/org/got5/tapestry5/jquery/services/RenderTrackerMixinWorker.java
+++ src/main/java/org/got5/tapestry5/jquery/services/RenderTrackerMixinWorker.java
@@ -4,28 +4,15 @@ import java.util.List;
 
 import org.apache.tapestry5.func.Predicate;
 import org.apache.tapestry5.model.MutableComponentModel;
-import org.apache.tapestry5.runtime.Component;
 import org.apache.tapestry5.services.ClassTransformation;
 import org.apache.tapestry5.services.ComponentClassTransformWorker;
-import org.apache.tapestry5.services.ComponentInstanceOperation;
-import org.apache.tapestry5.services.ComponentMethodAdvice;
-import org.apache.tapestry5.services.ComponentMethodInvocation;
-import org.apache.tapestry5.services.TransformField;
 import org.apache.tapestry5.services.TransformMethod;
-import org.apache.tapestry5.services.TransformMethodSignature;
 import org.got5.tapestry5.jquery.mixins.Selector;
 
 public class RenderTrackerMixinWorker implements ComponentClassTransformWorker {
-	private final RenderTracker renderTracker;
-	
-	public RenderTrackerMixinWorker(RenderTracker renderTracker) {
-		this.renderTracker = renderTracker;
-	}
 
 	public void transform(ClassTransformation transformation, MutableComponentModel model) {
-
-		List<TransformMethod> methods = transformation.matchMethods( new Predicate<TransformMethod>() {
-			
+		List<TransformMethod> methods = transformation.matchMethods( new Predicate<TransformMethod>() {			
 			@Override
 			public boolean accept(TransformMethod method) {
 				if ( "getClientId".equals(method.getName()) ) {
@@ -36,15 +23,8 @@ public class RenderTrackerMixinWorker implements ComponentClassTransformWorker {
 		});
 		if ( methods == null || methods.size() == 0 ) {
 			return;
-		}
-		for ( String mixin : model.getMixinClassNames() ) {
-			if ( mixin.equals(Selector.class.getName())) {
-				return;
-			}
-		}
-		
-		model.addMixinClassName(Selector.class.getName(),"before:*");
-				
+		}		
+		model.addMixinClassName(Selector.class.getName(),"before:*");				
 	}
 
 }
