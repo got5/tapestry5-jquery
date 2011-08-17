955e046d7dac2136e63b840e33007138cbbaa4ae
Barry Books <bfb@trsvax.com>
2011-08-17 05:38:33 -0500

    Fix Test cases
    Bind was using wrong zone class
    Changed selector to look for $ instead of jQeury

--- src/test/java/org/got5/tapestry5/jquery/test/pages/test/Bind.java
+++ src/test/java/org/got5/tapestry5/jquery/test/pages/test/Bind.java
@@ -1,22 +1,23 @@
 package org.got5.tapestry5.jquery.test.pages.test;
 
-import org.apache.tapestry5.annotations.Component;
+import org.apache.tapestry5.annotations.InjectComponent;
 import org.apache.tapestry5.annotations.OnEvent;
+import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.slf4j.Logger;
 
 public class Bind {
 	
-	@Component
-	private Zone clickZone;
+	@InjectComponent
+	@Property
+	private org.apache.tapestry5.corelib.components.Zone clickZone;
 	
 	@Inject
 	private Logger logger;
 	
 	@OnEvent(value="click")
 	Object onClick() {
-		logger.info("zone clicked");
-		return clickZone;
+		return clickZone.getBody();
 	}
 
 }
