41a21d8428a4ba4e9e6397c80ca73654ba63e0e3
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2011-08-24 10:56:14 +0200

    use .log instead of console.log

--- src/test/java/org/got5/tapestry5/jquery/test/BindTest.java
+++ src/test/java/org/got5/tapestry5/jquery/test/BindTest.java
@@ -11,7 +11,7 @@ public class BindTest extends SeleniumTestCase{
     {
 		open("/test/Bind");
 		
-		assertSourcePresent("jqbind","\"eventType\" : \"click\"");       
+		assertSourcePresent("jqbind","\"eventType\":\"click\"");       
     }
 	
 	@Test
