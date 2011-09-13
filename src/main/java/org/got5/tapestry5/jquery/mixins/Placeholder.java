93eec233f96cd30a343d98f913947fb2c39e3d5c
Pablo Nussembaum <baunax@gmail.com>
2011-09-13 16:19:22 -0300

    change to extend from Widget

--- src/main/java/org/got5/tapestry5/jquery/mixins/Placeholder.java
+++ src/main/java/org/got5/tapestry5/jquery/mixins/Placeholder.java
@@ -1,16 +1,10 @@
 package org.got5.tapestry5.jquery.mixins;
 
-import org.apache.tapestry5.ClientElement;
 import org.apache.tapestry5.MarkupWriter;
-import org.apache.tapestry5.annotations.Environmental;
 import org.apache.tapestry5.annotations.Import;
-import org.apache.tapestry5.annotations.InjectContainer;
 import org.apache.tapestry5.annotations.MixinAfter;
 import org.apache.tapestry5.annotations.Parameter;
-import org.apache.tapestry5.ioc.annotations.Inject;
-import org.apache.tapestry5.ioc.annotations.Symbol;
-import org.apache.tapestry5.services.javascript.JavaScriptSupport;
-import org.got5.tapestry5.jquery.JQuerySymbolConstants;
+import org.got5.tapestry5.jquery.mixins.ui.Widget;
 
 /**
  * A jQuery plugin that enables HTML5 placeholder support for legacy browsers.
@@ -22,37 +16,14 @@ import org.got5.tapestry5.jquery.JQuerySymbolConstants;
  */
 @Import(library = "${assets.path}/mixins/placeholder/jquery.placeholder.min.js")
 @MixinAfter
-public class Placeholder {
-
-    @Inject
-    @Symbol(JQuerySymbolConstants.JQUERY_ALIAS)
-    private String jqueryAlias;
-
+public class Placeholder extends Widget {
     /**
      * The format you want to use for your input.
      */
     @Parameter(allowNull = false, required = true)
     private String text;
 
-    /**
-     * The field component to which this mixin is attached.
-     */
-    @InjectContainer
-    private ClientElement element;
-
-    @Environmental
-    private JavaScriptSupport javaScriptSupport;
-
     void beginRender(MarkupWriter writer) {
         writer.attributes("placeholder", text);
-    }
-
-    /**
-     * Mixin afterRender phrase occurs after the component itself.
-     * 
-     * @param writer
-     */
-    void afterRender(MarkupWriter writer) {
-        javaScriptSupport.addScript("%s('#%s').placeholder();", jqueryAlias, element.getClientId());
     }
 }
