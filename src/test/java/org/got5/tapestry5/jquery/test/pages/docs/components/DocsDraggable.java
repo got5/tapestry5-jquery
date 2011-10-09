0dfcc64b37b188678bf3dae938cc5983f680ff84
got5 <fra.facon@gmail.com>
2011-10-09 14:49:27 +0200

    Draggable Component & ZoneDroppable Mixin

---
+++ src/test/java/org/got5/tapestry5/jquery/test/pages/docs/components/DocsDraggable.java
@@ -0,0 +1,61 @@
+//
+// Copyright 2010 GOT5 (GO Tapestry 5)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package org.got5.tapestry5.jquery.test.pages.docs.components;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.tapestry5.annotations.Component;
+import org.apache.tapestry5.annotations.Persist;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.corelib.components.Zone;
+import org.got5.tapestry5.jquery.utils.JQueryTabData;
+
+
+public class DocsDraggable
+{
+
+	@Persist
+	@Property
+	private String data;
+	
+	@Component
+	private Zone dropzone; 
+	
+	public List<JQueryTabData> getListTabData()
+	{
+		List<JQueryTabData> listTabData = new ArrayList<JQueryTabData>();
+		
+		listTabData.add(new JQueryTabData("Documentation","docs"));
+		
+	    listTabData.add(new JQueryTabData("Example","example"));
+	    
+	    return listTabData;
+
+	}
+
+	 public Object onDrop(String contexte)
+	 {
+		 data = contexte;
+		 return dropzone.getBody();		 
+	 }
+	 public Date getNow()
+	 {
+		 return new Date();
+	 }
+}
