17bcfc5f7117aba04b0f088ef111efa62c674dec
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2011-10-18 15:21:47 +0200

    Make the DialogLink component supporting Informal Parameters

--- src/main/java/org/got5/tapestry5/jquery/components/DialogLink.java
+++ src/main/java/org/got5/tapestry5/jquery/components/DialogLink.java
@@ -1,11 +1,13 @@
 package org.got5.tapestry5.jquery.components;
 
 import org.apache.tapestry5.BindingConstants;
+import org.apache.tapestry5.ComponentResources;
 import org.apache.tapestry5.MarkupWriter;
 import org.apache.tapestry5.annotations.AfterRender;
 import org.apache.tapestry5.annotations.BeginRender;
 import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.annotations.SetupRender;
+import org.apache.tapestry5.annotations.SupportsInformalParameters;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.json.JSONObject;
 import org.apache.tapestry5.services.AssetSource;
@@ -20,6 +22,7 @@ import org.got5.tapestry5.jquery.base.AbstractExtendableComponent;
  *
  * @see <a href="http://jqueryui.com/demos/dialog/">http://jqueryui.com/demos/dialog/</a>
  */
+@SupportsInformalParameters
 public class DialogLink extends AbstractExtendableComponent
 {
     /**
@@ -34,6 +37,9 @@ public class DialogLink extends AbstractExtendableComponent
     @Inject
     private AssetSource source;
 
+    @Inject
+    private ComponentResources resources;
+    
     private static final String[] scripts =
     { "org/got5/tapestry5/jquery/assets/components/dialoglink/dialoglink.js" };
 
@@ -53,6 +59,7 @@ public class DialogLink extends AbstractExtendableComponent
     @AfterRender
     void initJS(MarkupWriter writer)
     {
+    	resources.renderInformalParameters(writer);
         writer.end();
         
         JSONObject params = new JSONObject();
