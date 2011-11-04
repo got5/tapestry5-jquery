ec3ee90aebb6a4ef14e412812e9dc93a6dac0be8
Pablo Nussembaum <baunax@gmail.com>
2011-11-04 13:01:15 -0200

    call form form.validate only once.

--- src/main/resources/org/got5/tapestry5/jquery/validation.js
+++ src/main/resources/org/got5/tapestry5/jquery/validation.js
@@ -3,22 +3,25 @@
  * Container of functions that may be invoked by the Tapestry.init() function.
  */
 $.extend(Tapestry.Initializer, {
-    validate: function(/*field, */specs) {    	
+    validate: function(/*field, */specs) {
+    	var bForm = true;
         $.each(specs, function(i, ruleSpecs) {
             var field = i;
-            $('#' + field).closest('form').validate({
-                errorClass: "t-error"
-            });
+            if (bForm) {
+            	bForm = false;
+                $('#' + field).closest('form').validate({
+                    errorClass: "t-error"
+                });
+            }
+            
             $.each(ruleSpecs, function(j, ruleSpec) {
                 var jsRule = $.tapestry.validate.toJSRule(ruleSpec[0]);
                 var ruleAsString = '{"' + jsRule + '" :' + ((ruleSpec[2] === undefined) ? 'true' : '"' + ruleSpec[2] + '"') + ', "messages": {"' + jsRule + '":"' + ruleSpec[1] + '"}}';
                 $('#' + field).rules("add", $.parseJSON(ruleAsString));
-            });
-            
+            });            
         });
     }
 });
-
 
 /**
  * Tapestry static functions
