69cd48b548fcf33f668f22a7a496d86efe7cb7d6
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2011-12-22 15:50:07 +0100

    #113 - Bind Mixin - create show effect and update Integration Tests

--- src/test/java/org/got5/tapestry5/jquery/pages/Bind.java
+++ src/test/java/org/got5/tapestry5/jquery/pages/Bind.java
@@ -1,22 +1,18 @@
 package org.got5.tapestry5.jquery.pages;
 
-import org.apache.tapestry5.annotations.InjectComponent;
+import org.apache.tapestry5.annotations.Component;
 import org.apache.tapestry5.annotations.OnEvent;
 import org.apache.tapestry5.annotations.Property;
-import org.apache.tapestry5.ioc.annotations.Inject;
-import org.slf4j.Logger;
 
 public class Bind {
 	
-	@InjectComponent
+	@Component
 	@Property
 	private org.apache.tapestry5.corelib.components.Zone clickZone;
 	
-	@Inject
-	private Logger logger;
-	
 	@OnEvent(value="click")
 	Object onClick() {
+		System.out.println("############################");
 		return clickZone.getBody();
 	}
 
