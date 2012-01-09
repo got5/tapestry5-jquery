3c038aa96207368aa977ac1e4a7b0280b5d8054b
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2012-01-09 16:17:34 +0100

    Add Integration tests for DataTables component

--- src/test/java/org/got5/tapestry5/jquery/pages/DataTables.java
+++ src/test/java/org/got5/tapestry5/jquery/pages/DataTables.java
@@ -19,34 +19,18 @@ package org.got5.tapestry5.jquery.pages;
 import java.util.List;
 
 import org.apache.tapestry5.ComponentResources;
-import org.apache.tapestry5.annotations.AfterRender;
-import org.apache.tapestry5.annotations.Import;
-import org.apache.tapestry5.annotations.OnEvent;
 import org.apache.tapestry5.annotations.Property;
-import org.apache.tapestry5.annotations.RequestParameter;
 import org.apache.tapestry5.annotations.SessionState;
 import org.apache.tapestry5.beaneditor.BeanModel;
 import org.apache.tapestry5.grid.GridDataSource;
 import org.apache.tapestry5.ioc.annotations.Inject;
-import org.apache.tapestry5.ioc.services.TypeCoercer;
 import org.apache.tapestry5.json.JSONObject;
-import org.apache.tapestry5.services.AssetSource;
 import org.apache.tapestry5.services.BeanModelSource;
-import org.apache.tapestry5.services.TranslatorSource;
-import org.apache.tapestry5.services.javascript.JavaScriptSupport;
 import org.got5.tapestry5.jquery.data.Celebrity;
 import org.got5.tapestry5.jquery.data.CelebritySource;
 import org.got5.tapestry5.jquery.data.IDataSource;
-import org.got5.tapestry5.jquery.data.SampleDataTableModel;
-import org.got5.tapestry5.jquery.internal.DataTableModel;
 import org.got5.tapestry5.jquery.internal.TableInformation;
 
-@Import(stylesheet ={ "context:dataTables/css/demo_table_jui.css",
-					  "context:dataTables/css/demo_page.css",	
-					  "context:dataTables/css/demo_table.css",
-					  "context:dataTables/ColVis/media/css/ColVis.css", 
-					  "context:dataTables/ColReorder/media/css/ColReorder.css", 
-					  "context:dataTables/TableTools/css/TableTools.css"})
 public class DataTables
 {
 	@SessionState
@@ -97,14 +81,17 @@ public class DataTables
 		return new TableInformation() {
 			
 			public String getTableSummary() {
+				// TODO Auto-generated method stub
 				return "A summary description of table data";
 			}
 			
 			public String getTableCaption() {
-				return "The table Title";
+				// TODO Auto-generated method stub
+				return "The table title";
 			}
 			
 			public String getTableCSS() {
+				// TODO Auto-generated method stub
 				return "k-data-table";
 			}
 		};
@@ -113,50 +100,6 @@ public class DataTables
 	public JSONObject getOptions(){
 		
 		JSONObject json = new JSONObject("bJQueryUI", "true", "bStateSave", "true", "sDom", "TC<\"clear\">Rlfrtip");
-		
-		JSONObject dataTable = new JSONObject();
-		dataTable.put("sSwfPath", as.getContextAsset("dataTables/TableTools/swf/copy_cvs_xls_pdf.swf", null).toClientURL());
-		
-		json.put("oTableTools", dataTable);
-		
-		//These parameters are not a DataTable. They are used to get more information about a row
-		json.put("ajaxUrl", resources.createEventLink("extraInfo", null).toURI());
-		json.put("openImg", as.getContextAsset("img/details_open.png", null).toClientURL());
-		json.put("closeImg", as.getContextAsset("img/details_close.png", null).toClientURL());
-		
 		return json;
 	}
-	
-	@Inject
-	private JavaScriptSupport js;
-	
-	@Inject
-	private AssetSource as;
-	
-	
-	@AfterRender
-	public void addJsFile(){
-		js.importJavaScriptLibrary(as.getContextAsset("dataTables/ColVis/media/js/ColVis.js", null));
-		js.importJavaScriptLibrary(as.getContextAsset("dataTables/ColReorder/media/js/ColReorder.js", null));
-		js.importJavaScriptLibrary(as.getContextAsset("dataTables/TableTools/js/ZeroClipboard.js", null));
-		js.importJavaScriptLibrary(as.getContextAsset("dataTables/TableTools/js/TableTools.js", null));
-		js.importJavaScriptLibrary(as.getContextAsset("js/demo.js", null));
-	}
-	
-	@OnEvent(value="extrainfo")
-	public JSONObject sendResponse(@RequestParameter(value = "name") String name){
-		return new JSONObject("name", name);
-	}
-	
-	@Inject
-	private TypeCoercer typeCoercer;
-
-	@Inject
-	private TranslatorSource ts;
-	
-	
-	public DataTableModel getDataTableModel(){
-		return new SampleDataTableModel(typeCoercer, ts, resources);
-	}
-	
 }
