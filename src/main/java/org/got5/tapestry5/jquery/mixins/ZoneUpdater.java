1661bf40e6e3cb84a4fe72f0bcfd49136f9472ab
lltyk <lltyk@lltyk>
2012-05-21 13:20:10 -0400

    adding zoneupdate mixin

---
+++ src/main/java/org/got5/tapestry5/jquery/mixins/ZoneUpdater.java
@@ -0,0 +1,69 @@
+/**
+ * A simple mixin for attaching javascript that updates a zone on any client-side event.
+ * Based on http://tinybits.blogspot.com/2010/03/new-and-better-zoneupdater.html
+ */
+package org.got5.tapestry5.jquery.mixins;
+
+import org.apache.tapestry5.BindingConstants;
+import org.apache.tapestry5.ClientElement;
+import org.apache.tapestry5.ComponentResources;
+import org.apache.tapestry5.annotations.Environmental;
+import org.apache.tapestry5.annotations.Import;
+import org.apache.tapestry5.annotations.InjectContainer;
+import org.apache.tapestry5.annotations.Parameter;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.json.JSONObject;
+import org.apache.tapestry5.services.javascript.JavaScriptSupport;
+
+@Import(library = "${assets.path}/mixins/zoneupdater/zoneupdater.js")
+public class ZoneUpdater {
+
+  @Inject
+  private ComponentResources resources;
+
+  @Environmental
+  private JavaScriptSupport jsSupport;
+
+  /**
+   * The event to listen for on the client. If not specified, zone update can only be triggered manually through calling updateZone on the JS object.
+   */
+  @Parameter(defaultPrefix = BindingConstants.LITERAL)
+  private String clientEvent;
+
+  /**
+   * The event to listen for in your component class
+   */
+  @Parameter(defaultPrefix = BindingConstants.LITERAL, required = true)
+  private String event;
+
+  @Parameter(defaultPrefix = BindingConstants.LITERAL, value = "default")
+  private String prefix;
+
+  /**
+   * The element we attach ourselves to
+   */
+  @InjectContainer
+  private ClientElement element;
+
+  @Parameter
+  private Object[] context;
+
+  /**
+   * The zone to be updated by us.
+   */
+  @Parameter(defaultPrefix = BindingConstants.LITERAL, required = true)
+  private String zone;
+
+  void afterRender() {
+
+    String url = resources.createEventLink(event, context).toAbsoluteURI();
+    String elementId = element.getClientId();
+    JSONObject spec = new JSONObject();
+    spec.put("url", url);
+    spec.put("elementId", elementId);
+    spec.put("event", clientEvent);
+    spec.put("zone", zone);
+
+    jsSupport.addScript("%sZoneUpdater = new T5JQZoneUpdater(%s)", prefix, spec.toString());
+  }
+}
---
+++ src/test/java/org/got5/tapestry5/jquery/ZoneUpdaterTest.java
@@ -0,0 +1,30 @@
+package org.got5.tapestry5.jquery;
+
+import org.apache.tapestry5.test.SeleniumTestCase;
+import org.openqa.selenium.Keys;
+import org.testng.annotations.Test;
+
+import com.thoughtworks.selenium.Wait;
+
+public class ZoneUpdaterTest extends SeleniumTestCase {
+
+	@Test(groups="whatevs")
+	public void testZoneUpdater(){
+
+		open("/ZoneUpdater");
+		waitForPageToLoad();
+		assertTrue(isElementPresent("//div[@id='nameZone'][contains(text(), 'Humpty Dumpty')]"));
+		focus("//input[@type='text'][@id='firstName']");
+		type("//input[@type='text'][@id='firstName']", "Hempster");
+		keyUp("//input[@type='text'][@id='firstName']", Keys.CONTROL.toString());
+		focus("//input[@type='text'][@id='lastName']");
+		type("//input[@type='text'][@id='lastName']", "Dempster");
+		keyUp("//input[@type='text'][@id='lastName']", Keys.CONTROL.toString());
+		new Wait() {
+			@Override
+			public boolean until() {
+				return isElementPresent("//div[@id='nameZone'][contains(text(), 'Hempster Dempster')]");
+			}
+		}.wait("The tooltip is not visible", 50000000); //JQueryTestConstants.TIMEOUT)
+	}
+}
