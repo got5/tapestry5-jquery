ce123e925a0fe6dd39573468d06858d8fd6a33b4
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2012-09-24 10:35:52 +0200

    Delete a sysout

--- src/main/java/org/got5/tapestry5/jquery/components/ShowSource.java
+++ src/main/java/org/got5/tapestry5/jquery/components/ShowSource.java
@@ -120,8 +120,7 @@ public class ShowSource {
 		 * Init the Default parameter for the jQuery plugin
 		 */
 		defaultSpecs = new JSONObject();
-		System.out.println("########### " +  path.substring(path.lastIndexOf("/") + 1));
-		
+				
 		defaultSpecs.put("showMsg", path.substring(path.lastIndexOf("/") + 1) + " - " + message.get("ShowSource-showMsg"));
 			
 		defaultSpecs.put("hideMsg", path.substring(path.lastIndexOf("/") + 1) + " - " + message.get("ShowSource-hideMsg"));
