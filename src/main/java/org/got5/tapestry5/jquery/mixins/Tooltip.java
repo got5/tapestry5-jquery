067865122410e6bff4e6917aec8db50bd2906edd
Nourredine K. <nourredine.k@gmail.com>
2013-01-28 16:55:23 +0100

    Update src/main/java/org/got5/tapestry5/jquery/mixins/Tooltip.java
    fix javadoc

--- src/main/java/org/got5/tapestry5/jquery/mixins/Tooltip.java
+++ src/main/java/org/got5/tapestry5/jquery/mixins/Tooltip.java
@@ -1,18 +1,17 @@
 package org.got5.tapestry5.jquery.mixins;
 
 
+import org.apache.tapestry5.annotations.Import;
+import org.got5.tapestry5.jquery.ImportJQueryUI;
+import org.got5.tapestry5.jquery.mixins.ui.Widget;
+
 /**
  * This mixin allows you to display a tooltip for all your inputs. 
  * It use the title attribute of your element.
  * 
  * @since 2.1.1
  * @see <a href="http://access.aol.com/csun2011">http://access.aol.com/csun2011</a>
- */
-import org.apache.tapestry5.annotations.Import;
-import org.got5.tapestry5.jquery.ImportJQueryUI;
-import org.got5.tapestry5.jquery.mixins.ui.Widget;
-
-/**
+ *
  * @tapestrydoc
  */
 @ImportJQueryUI({ "jquery.ui.widget", "jquery.ui.core" })
