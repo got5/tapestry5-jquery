cefd960086e664e2d78ded11191b3e729ebb55af
Emmanuel <demey.emmanuel@gmail.com>
2013-05-21 09:57:29 +0200

    Issue #294 Add Conversion from Java Date Format to jQuery Date Format

--- src/main/java/org/got5/tapestry5/jquery/mixins/CustomDatepicker.java
+++ src/main/java/org/got5/tapestry5/jquery/mixins/CustomDatepicker.java
@@ -1,7 +1,10 @@
 package org.got5.tapestry5.jquery.mixins;
 
+import java.text.SimpleDateFormat;
+
 import org.apache.tapestry5.ClientElement;
 import org.apache.tapestry5.ComponentResources;
+import org.apache.tapestry5.annotations.BindParameter;
 import org.apache.tapestry5.annotations.Environmental;
 import org.apache.tapestry5.annotations.InjectContainer;
 import org.apache.tapestry5.annotations.Parameter;
@@ -56,6 +59,8 @@ public class CustomDatepicker {
 	@Symbol(JQuerySymbolConstants.JQUERY_ALIAS)
 	private String jQueryAlias;
 	
+	@BindParameter
+    private Object format;
 	/**
      * Mixin afterRender phrase occurs after the component itself. 
      * We will change the JSON option of the jQuery datepicker widget
@@ -71,7 +76,7 @@ public class CustomDatepicker {
          * We will call the WidgetParams in order to get the default JSON object for the CustomDatepicker mixin
          */
     	defaultParamsObject = widgetParams.paramsForWidget(this.getClass().getSimpleName().toLowerCase());
-    	
+    	defaultParamsObject.put("dateFormat", toJQueryUIDateFormat());
     	/*
     	 * We will merge the default JSON Object with the params parameter
     	 */
@@ -90,4 +95,62 @@ public class CustomDatepicker {
     	
     	javaScriptSupport.addInitializerCall(InitializationPriority.EARLY, "customDatepicker", json);
     }
+	
+	/**
+	 * 
+	 * ISSUE #294
+	 */
+	private String toJQueryUIDateFormat() {
+
+	    if (format == null) {
+	        return null;
+	    }
+	    else if (format instanceof SimpleDateFormat) {
+	        String pattern = ((SimpleDateFormat) format).toPattern();
+
+	        // Year
+	        if (pattern.contains("yyyy")) {
+	            pattern = pattern.replaceAll("yyyy", "yy");
+	        }
+	        else {
+	            pattern = pattern.replaceAll("yy", "y");
+	        }
+
+	        // Month
+	        if (pattern.contains("MMMM")) {
+	            pattern = pattern.replace("MMMM", "MM");
+	        }
+	        else if (pattern.contains("MMM")) {
+	            pattern = pattern.replace("MMM", "M");
+	        }
+	        else if (pattern.contains("MM")) {
+	            pattern = pattern.replace("MM", "mm");
+	        }
+	        else if (pattern.contains("M")) {
+	            pattern = pattern.replace("M", "m");
+	        }
+
+	        // Day
+	        if (pattern.contains("DD")) {
+	            pattern = pattern.replace("DD", "oo");
+	        }
+	        else if (pattern.contains("D")) {
+	            pattern = pattern.replace("D", "o");
+	        }
+
+	        // Day of month
+	        if (pattern.contains("EEEE")) {
+	            pattern = pattern.replace("EEEE", "DD");
+	        }
+	        else if (pattern.contains("EEE")) {
+	            pattern = pattern.replace("EEE", "D");
+	        }
+
+	        return pattern;
+	    }
+	    else {
+	        // Don't know how to extract the date pattern from this type of DateFormat.
+	        throw new IllegalArgumentException("Type is " + format.getClass().getName());
+	    }
+	}
 }
