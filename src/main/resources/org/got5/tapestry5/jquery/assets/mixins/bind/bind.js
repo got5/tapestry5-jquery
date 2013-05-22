028de3e3aeb3f7c4bb6f76b5f328d1dfdd3d7f91
Emmanuel <demey.emmanuel@gmail.com>
2013-05-22 09:45:48 +0200

    Issue #295 Bind Mixin sent automatically the value of the element

--- src/main/resources/org/got5/tapestry5/jquery/assets/mixins/bind/bind.js
+++ src/main/resources/org/got5/tapestry5/jquery/assets/mixins/bind/bind.js
@@ -35,9 +35,14 @@
 				}
 				
 				var u = new Object();
-				u.url = url;		
-				u.context = contextMarker;					
+				u.url = url;
+				
+				//If the element using the bind mixin has a value, we automatically added to tue url
+				if($(this).val() !== "") u.url += ("/" + $(this).val()) 	
+					
+				u.context = contextMarker;		
 				u.element = $(this);
+				
 				if ( history ) {
 					history(event,ui,u);
 				}
