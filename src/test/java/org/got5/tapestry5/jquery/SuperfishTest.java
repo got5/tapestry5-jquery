26198cdf4547c043a20b758d6172ff87021b09a1
Christian Riedel <cr.mlist@googlemail.com>
2013-06-03 00:44:58 +0200

    #299 wasn't working on firefox, unless mouseover is used instead of the click event

--- src/test/java/org/got5/tapestry5/jquery/SuperfishTest.java
+++ src/test/java/org/got5/tapestry5/jquery/SuperfishTest.java
@@ -6,12 +6,12 @@ import org.testng.annotations.Test;
 import com.thoughtworks.selenium.Wait;
 
 public class SuperfishTest extends SeleniumTestCase{
-	
+
 	@Test
     public void testSuperfishComponent(){
-    	
+
     	open("/SuperFish");
-    	
+
     	new Wait()
         {
             @Override
@@ -20,9 +20,9 @@ public class SuperfishTest extends SeleniumTestCase{
                 return isElementPresent("//ul[@id='superfish'][contains(@class,'sf-menu')]");
             }
         }.wait("The ul element should have the sf-menu class : " + getAttribute("//ul[@id='superfish']@class"), JQueryTestConstants.TIMEOUT);
-        
+
         click("//ul[@id='superfish']/li[1]/a");
-    	
+
         new Wait()
         {
             @Override
@@ -31,9 +31,9 @@ public class SuperfishTest extends SeleniumTestCase{
                 return getAttribute("//ul[@id='superfish']/li[1]@class").contains("sfHover");
             }
         }.wait("The ul element should have the sfHover class : " + getAttribute("//ul[@id='superfish'][contains(@class,'sf-menu')]/li[1]@class"), JQueryTestConstants.TIMEOUT);
-        
-        click("//ul[@id='superfish']/li[2]/a");
-        
+
+        mouseOver("//ul[@id='superfish']/li[2]/a");
+
         new Wait()
         {
             @Override
@@ -43,5 +43,5 @@ public class SuperfishTest extends SeleniumTestCase{
             }
         }.wait("The ul element should not have the sfHover class : " + getAttribute("//ul[@id='superfish'][contains(@class,'sf-menu')]/li[1]@class"), JQueryTestConstants.TIMEOUT);
     }
-	
+
 }
