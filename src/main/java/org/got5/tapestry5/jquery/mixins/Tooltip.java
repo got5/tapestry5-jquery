585e360382219154b187dec2107bc0f5f6b52371
Emmanuel <demey.emmanuel@gmail.com>
2013-07-09 11:32:45 +0200

    Merge Commit c2167a3185

--- src/main/java/org/got5/tapestry5/jquery/mixins/Tooltip.java
+++ src/main/java/org/got5/tapestry5/jquery/mixins/Tooltip.java
@@ -1,57 +1,54 @@
 package org.got5.tapestry5.jquery.mixins;
 
 
-import org.apache.tapestry5.BindingConstants;
 import org.apache.tapestry5.ClientElement;
+import org.apache.tapestry5.ComponentResources;
+import org.apache.tapestry5.MarkupWriter;
+import org.apache.tapestry5.annotations.AfterRender;
 import org.apache.tapestry5.annotations.InjectContainer;
-import org.apache.tapestry5.annotations.Parameter;
+import org.apache.tapestry5.annotations.MixinAfter;
+import org.apache.tapestry5.dom.Element;
 import org.apache.tapestry5.ioc.annotations.Inject;
-import org.apache.tapestry5.json.JSONObject;
-import org.apache.tapestry5.services.javascript.JavaScriptSupport;
-import org.got5.tapestry5.jquery.services.WidgetParams;
-import org.got5.tapestry5.jquery.utils.JQueryUtils;
+import org.apache.tapestry5.ioc.internal.util.InternalUtils;
+import org.got5.tapestry5.jquery.mixins.ui.Widget;
 
 /**
- * @see <a href="http://jqueryui.com/tooltip/">jQuery UI Official Documentation</a>
+ * This mixin allows you to display a tooltip for all your inputs.
+ * It use the title attribute of your element.
+ *
+ * You can specify the title in a properties file of your component/page using this mixin.
+ * You just need to specify a value for a key : [client-id]-title
+ *
+ *
+ * @since 2.1.1
+ * @see <a href="http://access.aol.com/csun2011">http://access.aol.com/csun2011</a>
+ *
  * @tapestrydoc
  */
-public class Tooltip {
-	/**
-	 * The JSON parameter for your widget
-	 */
-	@Parameter(defaultPrefix=BindingConstants.LITERAL)
-	private JSONObject options;
-	
-	
-	@InjectContainer
-	private ClientElement clientElement;
-	
-	@Inject
-	private JavaScriptSupport javaScriptSupport;
-	
-	@Inject
-	private WidgetParams widgetParams;
-		
-	void afterRender() {
-		JSONObject json = new JSONObject();
-		json.put("id", clientElement.getClientId());
-		json.put("options", overrideParams());
-		javaScriptSupport.require("tjq/ui").invoke("tooltip").with(json);
-				
-	}
-	
-	private JSONObject overrideParams(){
-		if ( options == null ) {
-			options = new JSONObject();
-		}
-		JSONObject params = new JSONObject();
-		String widgetName = this.getClass().getSimpleName().toLowerCase();
-		if(widgetParams.paramsForWidget(widgetName)!=null){
-			params=widgetParams.paramsForWidget(widgetName);
-			JQueryUtils.merge(params, options);
-			return params;
-		}
-		 
-		return options;
-	}
+@MixinAfter
+public class Tooltip extends Widget {
+
+    @InjectContainer
+	private ClientElement el;
+
+    @Inject
+    private ComponentResources cr;
+
+    private Element element;
+
+    void beginRender(MarkupWriter writer)
+    {
+        element = writer.getElement();
+    }
+
+    @AfterRender
+    void overrideTitle()
+    {
+
+        if (InternalUtils.isBlank(element.getAttribute("title")))
+        {
+            element.attribute("title", cr.getContainerResources().
+                    getContainerMessages().get(String.format("%s-title",el.getClientId())));
+        }
+    }
 }
