eb030debd1507b4b5bdba5333f5cadc6009fa552
Christian Riedel <cr.mlist@googlemail.com>
2013-08-27 15:49:15 +0200

    Merge Commit 2127ef43da

--- src/test/java/org/got5/tapestry5/jquery/DraggableTest.java
+++ src/test/java/org/got5/tapestry5/jquery/DraggableTest.java
@@ -5,50 +5,40 @@ import org.testng.annotations.Test;
 
 import com.thoughtworks.selenium.Wait;
 
-public class DraggableTest extends SeleniumTestCase{
 
-	@Test 
-	public void dragItem1(){
-		open("/draggable");
-		
-		new Wait()
-        {
+public class DraggableTest extends SeleniumTestCase {
+
+    @Test
+    public void dragItem1() {
+
+        open("/draggable");
+        waitForPageToLoad();
+        testDroppableZone("//div[@id='draggableItem1']", "contexteItem1");
+    }
+
+    @Test
+    public void dragItem2() {
+
+        open("/draggable");
+        waitForPageToLoad();
+        testDroppableZone("//div[@id='draggableItem2']", "contexteItem2");
+    }
+
+    private void testDroppableZone(final String item, final String value) {
+
+        new Wait() {
+
             @Override
-            public boolean until()
-            {
-                return isElementPresent("//div[@class='ui-droppable']");
-            }
-        }.wait("The Widget has not been initialized properly", JQueryTestConstants.TIMEOUT);
-        
-		dragAndDropToObject("//div[@id='draggableItem1']", "//div[@id='dropzone']");
-		testDroppableZone("contexteItem1");
-	}
-	
-	@Test 
-	public void dragItem2(){
-		open("/draggable");
-		
-		new Wait()
-        {
-            @Override
-            public boolean until()
-            {
-                return isElementPresent("//div[@class='ui-droppable']");
-            }
-        }.wait("The Widget has not been initialized properly", JQueryTestConstants.TIMEOUT);
-        
-		dragAndDropToObject("//div[@id='draggableItem2']", "//div[@id='dropzone']");
-		testDroppableZone("contexteItem2");
-	}
-	
-	private void testDroppableZone(final String value){
-		new Wait()
-        {
-            @Override
-            public boolean until()
-            {
+            public boolean until() {
+
+                /*
+                 * Selenium needs at least two drag'n'drops after page load for
+                 * some reason to trigger the drop event. I couldn't manually
+                 * reproduce this behaviour.
+                 */
+                dragAndDropToObject(item, "//div[@id='dropzone']");
                 return getText("//div[@id='dropzone']").contains(value);
             }
         }.wait("The Droppable Zone does not contain the right value", JQueryTestConstants.TIMEOUT);
-	}
+    }
 }
