768a32acce6870a1c78285675439fe01b86bc84c
Christian Riedel <cr.mlist@googlemail.com>
2013-08-27 15:17:51 +0200

    #262: fixed confirm and dialog tests by adding jquery.ui.menu as a dependency of the Autocomplete mixin

--- src/main/resources/org/got5/tapestry5/jquery/assets/mixins/confirm/confirm.js
+++ src/main/resources/org/got5/tapestry5/jquery/assets/mixins/confirm/confirm.js
@@ -4,46 +4,50 @@
 
 		function init(specs) {
 			
-			//JQuery dialog box configuration, if used.
-			if (!specs.useDefaultConfirm) {
-				var dialogBox = $('<div id=\'dialogConfirmationJQuery\' />').html(specs.message).dialog({
-					autoOpen : false,
-					resizable : specs.isResizable,
-					height : specs.height,
-					resize : 'auto',
-					title : specs.title,
-					modal : specs.isModal,
-					draggable : specs.isDraggable,
-					buttons : [ {
-						text : specs.validationMsg,
-						click : function() {
-							$(this).dialog("close");
-							
-							trigger($("#" + specs.id));
-						}
-					}, {
-						text : specs.cancelMsg,
-						click : function() {
-							$(this).dialog("close");
-						}
-					} ]
-				});
-			}
-			
-			$("#" + specs.id).click(function(event) {
-				if (specs.useDefaultConfirm) {
-					//Default javascript confirmation box.
-					return confirm(specs.message);
-				} else {
-					//JQuery confirmation box : we stop the event propagation before displaying the box.
-					//Otherwise the redirection will occur while confirmation box is displayed.
-					
-					event.preventDefault();
-					event.stopImmediatePropagation();
+		    $(document).ready(function () {
 
-					dialogBox.dialog("open");
-				}
-			});
+    			//JQuery dialog box configuration, if used.
+    			if (!specs.useDefaultConfirm) {
+    				var dialogBox = $('<div id="dialogConfirmationJQuery" />').html(specs.message).dialog({
+    					autoOpen : false,
+    					resizable : specs.isResizable,
+    					height : specs.height,
+    					resize : 'auto',
+    					title : specs.title,
+    					modal : specs.isModal,
+    					draggable : specs.isDraggable,
+    					buttons : [ {
+    						text : specs.validationMsg,
+    						click : function() {
+    							$(this).dialog('close');
+
+    							trigger($('#' + specs.id));
+    						}
+    					},
+    					{
+    						text : specs.cancelMsg,
+    						click : function() {
+    							$(this).dialog('close');
+    						}
+    					} ]
+    				});
+    			}
+
+    			$('#' + specs.id).click(function(event) {
+    				if (specs.useDefaultConfirm) {
+    					//Default javascript confirmation box.
+    					return confirm(specs.message);
+    				} else {
+    					//JQuery confirmation box : we stop the event propagation before displaying the box.
+    					//Otherwise the redirection will occur while confirmation box is displayed.
+
+    					event.preventDefault();
+    					event.stopImmediatePropagation();
+
+    					dialogBox.dialog('open');
+    				}
+    			});
+            });
 		}
 		
 		/**
@@ -52,14 +56,14 @@
 		 * @param HTML element
 		 */
 		function trigger(element) {
-			var tagName = element.prop("tagName");
+			var tagName = element.prop('tagName');
 			switch (tagName) {
 			//Simple link (pagelink, actionlink, etc...)
-			case "A":
-				var href = element.prop("href");
+			case 'A':
+				var href = element.prop('href');
 				if (href != undefined) {
 					var urlSuffix = href.substring(href.lastIndexOf('.') + 1);
-					if (urlSuffix != null && urlSuffix.toLowerCase().indexOf(element.prop("id").toLowerCase()) == 0) {
+					if (urlSuffix != null && urlSuffix.toLowerCase().indexOf(element.prop('id').toLowerCase()) == 0) {
 						//ActionLink
 						element.trigger(Tapestry.TRIGGER_ZONE_UPDATE_EVENT);
 					} else {
@@ -68,8 +72,8 @@
 				}
 				break;
 			//submit button.
-			case "INPUT":
-				element.parents("form").submit();
+			case 'INPUT':
+				element.parents('form').submit();
 				break;
 			default:
 				break;
