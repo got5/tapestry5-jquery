b14d77d7fac02390b12f31761b9e4f6ab6eecacf
Emmanuel <demey.emmanuel@gmail.com>
2013-09-27 15:41:37 +0200

    Migrate the ImageCropper demo page into Tap 5.4-alpha-22

--- src/test/java/org/got5/tapestry5/jquery/pages/ImageCropperPage.java
+++ src/test/java/org/got5/tapestry5/jquery/pages/ImageCropperPage.java
@@ -17,11 +17,7 @@
 package org.got5.tapestry5.jquery.pages;
 
 import org.apache.tapestry5.EventConstants;
-import org.apache.tapestry5.annotations.Component;
-import org.apache.tapestry5.annotations.OnEvent;
-import org.apache.tapestry5.annotations.Persist;
-import org.apache.tapestry5.annotations.Property;
-import org.apache.tapestry5.annotations.SetupRender;
+import org.apache.tapestry5.annotations.*;
 import org.apache.tapestry5.corelib.components.Zone;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.json.JSONArray;
