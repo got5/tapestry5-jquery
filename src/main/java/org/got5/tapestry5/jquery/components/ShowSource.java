c3d3cddb785ee49e84f208b9e98f7cd028bf5b30
Christian Riedel <cr.mlist@googlemail.com>
2013-11-27 11:47:02 +0100

    #318: replaced jquery.snippet.js with codemirror

--- src/main/java/org/got5/tapestry5/jquery/components/ShowSource.java
+++ src/main/java/org/got5/tapestry5/jquery/components/ShowSource.java
@@ -28,154 +28,158 @@ import org.slf4j.Logger;
 /**
  * Component for displaying a code source
  * @since 2.1.1
- * 
+ *
  * @tapestrydoc
  */
-@Import(library = {"${assets.path}/components/showSource/jquery.snippet.js",
-				  "${assets.path}/components/showSource/my-snippet.js"}, 
-		stylesheet = { "${assets.path}/components/showSource/jquery.snippet.css"})
+@Import(library = {
+            "${assets.path}/components/showSource/codemirror-compressed.js",
+            "${assets.path}/components/showSource/my-snippet.js"
+        },
+		stylesheet = {
+            "${assets.path}/components/showSource/codemirror.css"
+        })
 public class ShowSource {
-	
+
 	/**
 	 * Code Source path
 	 */
 	@Parameter(defaultPrefix=BindingConstants.LITERAL)
 	private String path;
-	
+
 	/**
 	 * Specs for the JQuery Plugin
 	 */
 	@Parameter(defaultPrefix=BindingConstants.PROP)
 	private JSONObject specs;
-	
+
 	/**
 	 * The ClientId of the ShowSource component
 	 */
 	@Parameter(value = "prop:componentResources.id", defaultPrefix = BindingConstants.LITERAL)
 	private String clientId;
-	
+
 	/**
 	 * The language you want to use for your snippet.
-	 * If not bound, we will use the extension of the file. 
+	 * If not bound, we will use the extension of the file.
 	 */
 	@Parameter(defaultPrefix=BindingConstants.LITERAL)
 	private String ext;
-	
+
 	/**
 	 * Where to Start ? By default line 0.
 	 */
 	@Parameter(value="0")
 	private Integer beginLine;
-	
+
 	/**
 	 * Where to finish ?
 	 */
-	@Parameter 
+	@Parameter
 	private Integer endLine;
-	
+
 	private JSONObject defaultSpecs;
-	
+
 	@Inject
 	private AssetSource assetSource;
-	
-	@Inject 
+
+	@Inject
 	private Logger logger;
-	
+
 	@Inject
     private JavaScriptSupport support;
-	
+
 	private Map<String, String> langs;
-	
+
 	@Inject
     @Symbol("demo-src-dir")
     private String srcDir;
-	
-	@Inject 
+
+	@Inject
 	private ComponentResources componentResources;
-	
-	@Inject 
+
+	@Inject
 	private Messages message;
-	
+
 	private boolean setupRender()
 	{
 		if(!componentResources.isBound("path")){
-			
+
 			logger.warn("We have to specify a path " +
 					"for the showSource component");
-			
+
 			return false;
 		}
-		
+
 		if(componentResources.isBound("endLine"))
 		{
 			if(endLine<beginLine)
 			{
 				logger.warn("The endLine parameter has to be greater than beginLine");
-				
+
 				return false;
 			}
 		}
-		
+
 		/**
 		 * Init the Default parameter for the jQuery plugin
 		 */
 		defaultSpecs = new JSONObject();
-				
+
 		defaultSpecs.put("showMsg", path.substring(path.lastIndexOf("/") + 1) + " - " + message.get("ShowSource-showMsg"));
-			
+
 		defaultSpecs.put("hideMsg", path.substring(path.lastIndexOf("/") + 1) + " - " + message.get("ShowSource-hideMsg"));
-		
+
 		defaultSpecs.put("style", message.get("ShowSource-style"));
-			
+
 		defaultSpecs.put("collapse", message.get("ShowSource-collapse"));
-			
+
 		defaultSpecs.put("showNum", message.get("ShowSource-showNum"));
-		
+
 		defaultSpecs.put("clipboard", assetSource.getUnlocalizedAsset("org/got5/tapestry5/jquery/assets/components/showSource/my-snippet.js").toClientURL());
-		
+
 		return true;
 	}
-	
-	public String getSrcContent() 
+
+	public String getSrcContent()
 	{
 		StringBuffer buffer = new StringBuffer();
-		
-		InputStream is = null;	
-		
+
+		InputStream is = null;
+
 		File file = null;
-		
-		String rootSrc = InternalUtils.isBlank(srcDir) ? System.getProperty("user.dir")+"/src/test/" : srcDir; 
-		
+
+		String rootSrc = InternalUtils.isBlank(srcDir) ? System.getProperty("user.dir")+"/src/test/" : srcDir;
+
 		String pathFile = rootSrc+File.separator+path;
-		
+
 		logger.info("The ShowSource Component displays the file : " + pathFile);
-		
+
 		file = new File(pathFile);
 
-		try 
+		try
 		{
 			is = new FileInputStream(file);
-		} 
-		catch (FileNotFoundException fnfEx) 
+		}
+		catch (FileNotFoundException fnfEx)
 		{
 			logger.error("Error file.");
 		}
-		
-		if (is != null) 
-		{	
-			try 
+
+		if (is != null)
+		{
+			try
 			{
 				Integer numLine = 1;
-				
+
 				BufferedReader buffReader = new BufferedReader(new InputStreamReader(is));
-				
+
 				String line = buffReader.readLine();
-				
-				while (line!=null) 
+
+				while (line!=null)
 				{
 					if(numLine>=beginLine)
 					{
-							
+
 						if(componentResources.isBound("endLine") && numLine>endLine){
 							break;
 						}
@@ -183,26 +187,26 @@ public class ShowSource {
 							buffer.append(new String(new byte[] { Character.LINE_SEPARATOR }));
 						}
 						buffer.append(line);
-						
+
 					}
-					
+
 					numLine++;
-					
+
 					line = buffReader.readLine();
 				}
-				
+
 				buffReader.close();
-				
-			} 
-			catch (IOException ioEx) 
+
+			}
+			catch (IOException ioEx)
 			{
 				buffer.append(ioEx.getMessage());
-			} 
-			finally 
+			}
+			finally
 			{
 				if(is != null)
 				{
-					try 
+					try
 					{
 						is.close();
 					}
@@ -213,51 +217,56 @@ public class ShowSource {
 				}
 			}
 		}
-		
+
 		return buffer.toString();
-		
+
 	}
-	
+
 	public void afterRender()
 	{
 		JSONObject params = new JSONObject();
-		
+
 		params.put("id", getClientId());
-		
+
 		params.put("lang", getLanguage());
-		
+
 		params.put("beginLine", beginLine);
-		
+
 		JQueryUtils.merge(defaultSpecs,specs);
-		
+
 		defaultSpecs.put("collapse", Boolean.parseBoolean(defaultSpecs.getString("collapse")));
-		
+
 		defaultSpecs.put("showNum", Boolean.parseBoolean(defaultSpecs.getString("showNum")));
-		
+
 		JQueryUtils.merge(params, defaultSpecs);
-		
+
 		support.addInitializerCall("source", params);
-		
+
 	}
-	
+
 	public String getLanguage(){
-		
+
 		if(componentResources.isBound("ext"))
 			return ext;
-		
+
 		langs = new HashMap<String, String>();
-			
+
 		langs.put("js", "javascript");
-		
+
 		langs.put("tml", "html");
-			
-		String extension  = path.substring((path.lastIndexOf('.')+1)); 
-			
+
+		String extension  = path.substring((path.lastIndexOf('.')+1));
+
 		return extension = langs.get(extension)!=null ? langs.get(extension) : extension;
-		
+
 	}
-	
+
 	public String getClientId() {
 		return clientId;
+	}
+
+	public String getFilename() {
+
+	    return path.substring(path.lastIndexOf("/") + 1);
 	}
 }
--- src/main/resources/org/got5/tapestry5/jquery/assets/components/showSource/my-snippet.js
+++ src/main/resources/org/got5/tapestry5/jquery/assets/components/showSource/my-snippet.js
@@ -1,73 +1,19 @@
-(function($){
-    
-	T5.extendInitializers(function(){
-		
+(function($) {
+
+	T5.extendInitializers(function() {
+
 		function init(specs) {
-var snippet = $("#"+specs.id + " pre."+specs.lang);
-        	
-        	$(snippet).snippet(specs.lang,
-        	{
-        		style:specs.style,
-        		collapse:specs.collapse,
-        		showMsg:specs.showMsg,
-        		hideMsg:specs.hideMsg,
-        		showNum:specs.showNum,
-        		clipboard:specs.clipboard
-        	});
-        	
-        	if(specs.beginLine!=0)
-        	{
-	        	var count=0;
-	        	
-	        	var goodCount=0;
-	        	
-	        	var flag=false;
-	        	
-	        	$(snippet).find("li").each(function(index){
-	        		
-	        		var o = $(this);
-	        		
-	        		o.html(o.html().replace(/&nbsp;/gi,' '));
-	        		
-	        		if(!/^\s*$/.test(o.html()))
-	        		{
-		        		for(var i=0; i<o.html().length; i++)
-			        	{	
-		        			
-			        		if(o.html().charAt(i)==' ')
-			        		{
-			        			count++;
-			        		}
-			        		else
-			        		{
-			        			break;
-			        		}
-			        	}
-	        			
-			        	if(count<goodCount || !flag) {goodCount=count;flag=true;}
-			        	
-			        	count=0;
-	        		}
-	        		
-	        	});
-	        	
-	        	$(snippet).find("li").each(function(index){
-	        		
-	        		var o = $(this);
-	        		
-	        		o.html(o.html().substring(goodCount));
-	        	});
-        	}
-        	
-        	$(snippet).closest(".my-snippet-container").css("display", "block");
-        	
-        
+		    var snippet = $('#' + specs.id);
+
+            var editor = CodeMirror.fromTextArea(document.getElementById(specs.id), { 
+                lineNumbers: true,
+                firstLineNumber: specs.beginLine !== 0 ? specs.beginLine : 1 
+            });
 		}
-		
+
 		return {
 			source : init
 		};
 	});
-    
-    
+
 })(jQuery);
