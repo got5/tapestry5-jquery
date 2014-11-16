9ea31cfff63970f9474c654433fc9f64220ced1c
ffacon <francois.facon@gmail.com>
2014-11-16 13:07:06 +0100

    Fix Layout navbar and xsd

--- src/test/java/org/got5/tapestry5/jquery/components/Layout.java
+++ src/test/java/org/got5/tapestry5/jquery/components/Layout.java
@@ -1,5 +1,5 @@
 //
-// Copyright 2010 GOT5 (Gang Of Tapestry 5)
+// Copyright 2010-2014 GOT5 (Gang Of Tapestry 5)
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
