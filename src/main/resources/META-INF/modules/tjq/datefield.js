aff1ff59a796053153112e15b0ee02400514dc2f
ffacon <francois.facon@gmail.com>
2015-01-10 15:41:08 +0100

    Add dependencies order in order to force the load of datepicker local after others ui dependencies

--- src/main/resources/META-INF/modules/tjq/datefield.js
+++ src/main/resources/META-INF/modules/tjq/datefield.js
@@ -1,7 +1,20 @@
 (function() {
 	 locale = (document.documentElement.getAttribute("data-locale")) || "en";	
-	 var datepickerLocal= "tjq/vendor/ui/i18n/jquery.ui.datepicker-"+locale;
-define(["t5/core/dom", "t5/core/events","tjq/vendor/ui/jquery.ui.datepicker", "tjq/vendor/ui/jquery-ui.custom",datepickerLocal], function(dom, events) {
+	 requirejs.config({
+		 	paths: {
+		 		"datepickerLocal" : "tjq/vendor/ui/i18n/jquery.ui.datepicker-"+locale
+		 		},
+			shim : {
+				"tjq/vendor/ui/jquery.ui.datepicker": ["jquery"],
+				"tjq/vendor/ui/jquery-ui.custom": ["tjq/vendor/ui/jquery.ui.datepicker"],
+				"datepickerLocal": 
+					{
+						deps: ["tjq/vendor/ui/jquery-ui.custom","tjq/vendor/ui/jquery.ui.datepicker"]
+					}
+				}
+	 });
+	 
+define(["t5/core/dom", "t5/core/events","datepickerLocal","tjq/vendor/ui/jquery-ui.custom","tjq/vendor/ui/jquery.ui.datepicker"], function(dom, events) {
 
   scan = function(root) {
     var container, _i, _len, _ref, _results, field;
@@ -14,7 +27,7 @@ define(["t5/core/dom", "t5/core/events","tjq/vendor/ui/jquery.ui.datepicker", "t
       container.attr("data-component-type", null);
       
       container.findFirst("button").remove();
-      field = container.findFirst("input").$
+      field = jQuery(container.findFirst("input").element);
 
       //jQuery.datepicker.setDefaults(jQuery.datepicker.regional[locale]);
       params = jQuery.extend({
