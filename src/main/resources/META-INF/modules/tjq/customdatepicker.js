6d9c642946317966305241daed924a46a8097eb4
ffacon <francois.facon@gmail.com>
2015-01-11 13:06:10 +0100

    make sure datepicker locale is loaded after others jquery ui dependencies for customdatepicker

--- src/main/resources/META-INF/modules/tjq/customdatepicker.js
+++ src/main/resources/META-INF/modules/tjq/customdatepicker.js
@@ -1,8 +1,20 @@
 (function() {
 		 locale = (document.documentElement.getAttribute("data-locale")) || "en";	
-		 var datepickerLocal= "tjq/vendor/ui/i18n/jquery.ui.datepicker-"+locale;
-
-define(["t5/core/dom", "t5/core/events","tjq/vendor/ui/jquery.ui.datepicker", "tjq/vendor/ui/jquery-ui.custom",datepickerLocal], function() {
+		 requirejs.config({
+			 	paths: {
+			 		"datepickerLocal" : "tjq/vendor/ui/i18n/jquery.ui.datepicker-"+locale
+			 		},
+				shim : {
+					"tjq/vendor/ui/jquery.ui.datepicker": ["jquery"],
+					"tjq/vendor/ui/jquery-ui.custom": ["tjq/vendor/ui/jquery.ui.datepicker"],
+					"datepickerLocal": 
+						{
+							deps: ["tjq/vendor/ui/jquery-ui.custom","tjq/vendor/ui/jquery.ui.datepicker"]
+						}
+					}
+		 });
+		 
+define(["t5/core/dom", "t5/core/events","tjq/vendor/ui/jquery.ui.datepicker", "tjq/vendor/ui/jquery-ui.custom","datepickerLocal"], function() {
 	init = function(spec) {
 		
 		jQuery.datepicker.setDefaults(jQuery.datepicker.regional[spec.locale]);
