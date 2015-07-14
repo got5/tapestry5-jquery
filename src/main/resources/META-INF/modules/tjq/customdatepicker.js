587140462d7101714fec5a10fc02cde63aa3e148
ffacon <francois.facon@gmail.com>
2015-07-14 21:41:01 +0200

    Update customdatepicker.js

--- src/main/resources/META-INF/modules/tjq/customdatepicker.js
+++ src/main/resources/META-INF/modules/tjq/customdatepicker.js
@@ -5,10 +5,10 @@
             "datepickerLocal" : "tjq/vendor/ui/i18n/jquery.ui.datepicker-"+locale
         },
         shim : {
-            "tjq/vendor/ui/jquery.ui.datepicker": ["jquery"],
-            "tjq/vendor/ui/jquery-ui.custom": ["tjq/vendor/ui/jquery.ui.datepicker"],
+            "tjq/vendor/ui/datepicker": ["jquery"],
+            "tjq/vendor/ui/custom": ["tjq/vendor/ui/datepicker"],
             "datepickerLocal": {
-                deps: ["tjq/vendor/ui/jquery-ui.custom","tjq/vendor/ui/jquery.ui.datepicker"]
+                deps: ["tjq/vendor/ui/custom","tjq/vendor/ui/datepicker"]
             }
         }
     });
