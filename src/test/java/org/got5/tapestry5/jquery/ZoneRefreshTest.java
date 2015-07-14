b2c608ffebd5f02e748ac155471a488c301bc6a8
ffacon <francois.facon@gmail.com>
2015-07-15 00:57:05 +0200

    Fix ZoneRefreshTest

--- src/test/java/org/got5/tapestry5/jquery/ZoneRefreshTest.java
+++ src/test/java/org/got5/tapestry5/jquery/ZoneRefreshTest.java
@@ -8,62 +8,37 @@ import com.thoughtworks.selenium.Wait;
 public class ZoneRefreshTest extends SeleniumTestCase{
 
 	@Test
-	public void testZoneRefresh(){
+	public void testZoneRefresh() throws Exception{
 		
 		open("/ZoneRefresh");
 		
-		new Wait()
-		{
-			@Override
-		    public boolean until()
-		    {
-				return getText("//div[@id='clickZone']").equals("0");
-		    }
-		}.wait("The ZoneRefresh mixin does not work well : expected 0 : " + getText("//div[@id='clickZone']"), 
-				JQueryTestConstants.TIMEOUT);
 		
-		new Wait()
-		{
-			@Override
-		    public boolean until()
-		    {
-				return getText("//div[@id='clickZone']").equals("1");
-		    }
-		}.wait("The ZoneRefresh mixin does not work well : expected 0 : " + getText("//div[@id='clickZone']"), 
-				JQueryTestConstants.TIMEOUT);
-		
-		new Wait()
-		{
-			@Override
-		    public boolean until()
-		    {
-				return getText("//div[@id='clickZone']").equals("2");
-		    }
-		}.wait("The ZoneRefresh mixin does not work well : expected 0 : " + getText("//div[@id='clickZone']"), 
-				JQueryTestConstants.TIMEOUT);
-		
+		Thread.sleep(1000l);
 		click("//a[@id='stop']");
-		
+		Thread.sleep(1000l);
+		final String count = getText("//div[@id='clickZone']");
+		Thread.sleep(1000l);
 		new Wait()
 		{
 			@Override
 		    public boolean until()
 		    {
-				return getText("//div[@id='clickZone']").equals("2");
+				return getText("//div[@id='clickZone']").equals(count);
 		    }
-		}.wait("The Stop event of the ZoneRefresh mixin does not work well : expected 0 : " + getText("//div[@id='clickZone']"), 
+		}.wait("The Stop event of the ZoneRefresh mixin does not work well : expected "+count+" : " + getText("//div[@id='clickZone']"), 
 				JQueryTestConstants.TIMEOUT);
 		
 		click("//a[@id='start']");
+		Thread.sleep(1000l);
 		
 		new Wait()
 		{
 			@Override
 		    public boolean until()
 		    {
-				return getText("//div[@id='clickZone']").equals("3");
+				return !getText("//div[@id='clickZone']").equals(count);
 		    }
-		}.wait("The Start event of the ZoneRefresh mixin does not work well : expected 0 : " + getText("//div[@id='clickZone']"), 
+		}.wait("The Start event of the ZoneRefresh mixin does not work well : not expected "+count+" : " + getText("//div[@id='clickZone']"), 
 				JQueryTestConstants.TIMEOUT);
 	}
 }
