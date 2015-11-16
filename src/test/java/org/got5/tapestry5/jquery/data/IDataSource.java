6d0e1f46c756fda656c72495056b6f30ec99e58b
ffacon <francois.facon@gmail.com>
2015-11-16 09:27:51 +0100

    Create a new constructor

--- src/test/java/org/got5/tapestry5/jquery/data/IDataSource.java
+++ src/test/java/org/got5/tapestry5/jquery/data/IDataSource.java
@@ -5,6 +5,7 @@ import java.util.List;
 public interface IDataSource {
 	public List<Celebrity> getAllCelebrities();
     public Celebrity getCelebrityById(long id);
+    public void deleteCelebrity(long id);
     public void addCelebrity(Celebrity c);
     public List<Celebrity> getRange(int indexFrom, int indexTo);
     public void filter(String value);
