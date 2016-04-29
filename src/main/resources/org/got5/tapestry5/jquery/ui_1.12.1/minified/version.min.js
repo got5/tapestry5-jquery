be8174a43e10d011f9cddf4108fae07f4d204dc5
ffacon <francois.facon@gmail.com>
2016-04-29 11:43:38 +0200

    Update Minified ui version

--- src/main/resources/org/got5/tapestry5/jquery/ui_1.12.1/minified/version.min.js
+++ src/main/resources/org/got5/tapestry5/jquery/ui_1.12.1/minified/version.min.js
@@ -1,17 +1,1 @@
-( function( factory ) {
-	if ( typeof define === "function" && define.amd ) {
-
-		// AMD. Register as an anonymous module.
-		define( [ "jquery" ], factory );
-	} else {
-
-		// Browser globals
-		factory( jQuery );
-	}
-} ( function( $ ) {
-
-$.ui = $.ui || {};
-
-return $.ui.version = "1.12.1";
-
-} ) );
+!function(e){"function"==typeof define&&define.amd?define(["jquery"],e):e(jQuery)}(function(e){return e.ui=e.ui||{},e.ui.version="1.12.1"});
