3474fcb8a2d4eeb71664ac263a73260282f11c7a
ffacon <francois.facon@gmail.com>
2016-07-04 17:40:30 +0200

    Update DataTableConstants

--- src/main/java/org/got5/tapestry5/jquery/DataTableConstants.java
+++ src/main/java/org/got5/tapestry5/jquery/DataTableConstants.java
@@ -1,19 +1,65 @@
 package org.got5.tapestry5.jquery;
 
 public class DataTableConstants {
-	
-	 public static final String ECHO = "sEcho";
-		
-	 public static final String SEARCH = "sSearch";
+		  
+	 /**
+	  * @deprecated no more used in version >1.10.
+	  * see https://datatables.net/manual/server-side
+	  */
+	 @Deprecated
+	 public static final String ECHO = "echo";
+	 /**
+	  * @deprecated Use LENGTH instead.
+	  */
+	 public static final String SEARCH = "search"; 
+	 /**
+	  * @deprecated Use LENGTH instead.
+	  */
+	 @Deprecated
+	 public static final String DISPLAY_LENGTH = "displayLength";
+	 /**
+	  * @deprecated Use START instead.
+	  */
+	 @Deprecated
+	 public static final String DISPLAY_START = "sDdisplayStart";
 	 
-	 public static final String DISPLAY_LENGTH = "iDisplayLength";
-	 
-	 public static final String DISPLAY_START = "iDisplayStart";
-	 
+	 /**
+	  * @deprecated no more used in version >1.10.
+	  * see https://datatables.net/manual/server-side
+	  */
+	 @Deprecated
 	 public static final String SORTING_COLS = "iSortingCols";
 	 
+	 /**
+	  * @deprecated Use ORDER_IDX instead.
+	  */
+	 @Deprecated
 	 public static final String SORT_COL = "iSortCol_";
 	 
+	 /**
+	  * @deprecated Use ORDER_DIR instead.
+	  */
+	 @Deprecated
 	 public static final String SORT_DIR = "sSortDir_";
+	 
+	 public static final String DATA = "data";
+	 
+	 public static final String DRAW = "draw";
+	 
+	 public static final String SEARCH_VALUE = "search[value]";
+	 
+	 public static final String SEARCH_REGEX = "search[regex]";
+	 
+	 public static final String RECORDS_TOTAL = "recordsTotal";
+	 
+	 public static final String RECORDS_FILTERED = "recordsFiltered";
+	 
+	 public static final String ORDER_DIR = "order[0][dir]";
+	 
+	 public static final String ORDER_IDX = "order[0][column]";
+	 
+	 public static final String LENGTH = "length";
+	 
+	 public static final String START = "start";
 	 
 }
