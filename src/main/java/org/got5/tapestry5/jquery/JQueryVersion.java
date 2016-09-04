1bbe0b4b319686f5a8a061e0510866362920de15
ffacon <francois.facon@gmail.com>
2016-09-04 10:34:31 +0200

    Update version definition

--- src/main/java/org/got5/tapestry5/jquery/JQueryVersion.java
+++ src/main/java/org/got5/tapestry5/jquery/JQueryVersion.java
@@ -9,9 +9,10 @@ public class JQueryVersion {
     /**
      * Compatible with older IEs, default.
      */
-    public static final String CORE_1_10_2 = "1.10.2";
+     
+    public static final String CORE_1_12_4 = "1.12.4";
 
     public static final String CORE_2_0_3 = "2.0.3";
-
-    public static final String UI_1_10_3 = "1.10.3";
+    
+    public static final String UI_1_12_1 = "1.12.1";
 }
