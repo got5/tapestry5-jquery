42576b718b906dd8534f28ee62b61a687457ab4e
ffacon <francois.facon@gmail.com>
2016-09-19 10:42:02 +0200

    update ImportjQueryUIWithTheme

--- src/test/java/org/got5/tapestry5/jquery/pages/annotations/ImportJQueryUIWithTheme.java
+++ src/test/java/org/got5/tapestry5/jquery/pages/annotations/ImportJQueryUIWithTheme.java
@@ -2,7 +2,7 @@ package org.got5.tapestry5.jquery.pages.annotations;
 
 import org.got5.tapestry5.jquery.ImportJQueryUI;
 
-@ImportJQueryUI(theme="classpath:org/got5/tapestry5/jquery/themes/ui-lightness/jquery-ui-1.8.19.custom.css")
+@ImportJQueryUI(theme="classpath:org/got5/tapestry5/jquery/themes/1.12.1/ui-lightness/jquery-ui.css")
 public class ImportJQueryUIWithTheme extends AbstractImportJQueryUI {
 
 }
