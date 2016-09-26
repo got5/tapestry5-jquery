f45dbd241ffe3250b4c5f411f8cf9b1d5b5b483b
ffacon <francois.facon@gmail.com>
2016-09-26 10:51:05 +0200

    Update Dialog

--- src/main/java/org/got5/tapestry5/jquery/components/Dialog.java
+++ src/main/java/org/got5/tapestry5/jquery/components/Dialog.java
@@ -19,14 +19,27 @@ import org.got5.tapestry5.jquery.utils.JQueryUtils;
  * There are a few components you can use to create a dialog in your pages.
  *  - Dialog, this is the base of your Dialog, just put the content of your dialog inside.
  * 	- DialogLink, clicking a DialogLink will make the associated Dialog to open.
- *	- DialogAjaxLink, clicking a DialogAjaxLink will open the associated Dialog and refresh the zone you've set inside the Dalog.
+ *	- DialogAjaxLink, clicking a DialogAjaxLink will open the associated Dialog and refresh the zone you've set inside the Dialog.
  *
  * @see <a href="http://jqueryui.com/demos/dialog/">http://jqueryui.com/demos/dialog/</a>
  *
  * @tapestrydoc
  */
 @SupportsInformalParameters
-@ImportJQueryUI(value = { "jquery.ui.mouse", "jquery.ui.draggable", "jquery.ui.resizable", "jquery.ui.button", "jquery.ui.dialog" })
+@ImportJQueryUI(value = { "focusable",
+						  "keycode",
+						  "position",
+						  "safe-active-element",
+						  "safe-blur",
+						  "tabbable",
+						  "unique-id",
+						  "version",
+						  "keycode",
+						  "data",
+						  "plugin",
+						  "scroll-parent",
+						  "widget",	
+						  "widgets/mouse",  "widgets/draggable", "widgets/resizable", "widgets/button", "widgets/dialog" })
 public class Dialog implements ClientElement
 {
     /**
