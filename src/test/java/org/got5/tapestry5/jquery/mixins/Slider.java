946a5e645619119f32c7a78a9c3429efb65bef6f
ffacon <francois.facon@gmail.com>
2016-10-24 11:03:31 +0200

    Update Slider

--- src/test/java/org/got5/tapestry5/jquery/mixins/Slider.java
+++ src/test/java/org/got5/tapestry5/jquery/mixins/Slider.java
@@ -3,7 +3,12 @@ package org.got5.tapestry5.jquery.mixins;
 import org.got5.tapestry5.jquery.ImportJQueryUI;
 import org.got5.tapestry5.jquery.mixins.ui.Widget;
 
-@ImportJQueryUI(value = {"jquery.ui.widget", "jquery.ui.mouse", "jquery.ui.slider"})
+@ImportJQueryUI(value = {"ie",
+						 "version",
+						 "keycode",
+						 "widget", 
+						 "widgets/mouse", 
+						 "widgets/slider"})
 public class Slider extends Widget {
 
 }
