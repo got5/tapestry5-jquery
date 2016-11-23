7ef3a1355fe2e6efa9136929bba9312609e65ade
ffacon <francois.facon@gmail.com>
2016-11-23 22:33:09 +0100

    Add minified ui file without min suffixe

---
+++ src/main/resources/org/got5/tapestry5/jquery/ui_1.12.1/minified/tabbable.js
@@ -0,0 +1,1 @@
+!function(e){"function"==typeof define&&define.amd?define(["jquery","./version","./focusable"],e):e(jQuery)}(function(e){return e.extend(e.expr[":"],{tabbable:function(n){var t=e.attr(n,"tabindex"),u=null!=t;return(!u||t>=0)&&e.ui.focusable(n,u)}})});
