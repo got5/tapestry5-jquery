fb155f2a780b6591934dde68b713f82ea69fb09e
ffacon <francois.facon@gmail.com>
2016-12-01 22:50:39 +0100

    Remove ui scripts with min suffixe

--- src/main/resources/org/got5/tapestry5/jquery/ui_1.12.1/minified/core.min.js
+++
@@ -1,1 +0,0 @@
-!function(){define(["jquery","./data","./disable-selection","./focusable","./form","./ie","./keycode","./labels","./jquery-1-7","./plugin","./safe-active-element","./safe-blur","./scroll-parent","./tabbable","./unique-id","./version"])}();
--- src/main/resources/org/got5/tapestry5/jquery/ui_1.12.1/minified/unique-id.min.js
+++
@@ -1,1 +0,0 @@
-!function(i){"function"==typeof define&&define.amd?define(["jquery","./version"],i):i(jQuery)}(function(i){return i.fn.extend({uniqueId:function(){var i=0;return function(){return this.each(function(){this.id||(this.id="ui-id-"+ ++i)})}}(),removeUniqueId:function(){return this.each(function(){/^ui-id-\d+$/.test(this.id)&&i(this).removeAttr("id")})}})});
