0053a1581d20c53eb1b4961adbc5cfd3fcdb30a3
ffacon <francois.facon@gmail.com>
2016-12-02 22:54:01 +0100

    remove min extension in gulp tasks

--- gulp/GulpFile.js
+++ gulp/GulpFile.js
@@ -7,27 +7,23 @@ var dest = require('gulp-dest');
 gulp.task('compressjs', function () {
   gulp.src('../src/main/resources/org/got5/tapestry5/jquery/ui_1.12.1/*.js')
     .pipe(uglify())
-    .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('js'));
 })
 
 gulp.task('compresswidgets', function () {
   gulp.src('../src/main/resources/org/got5/tapestry5/jquery/ui_1.12.1/widgets/*.js')
     .pipe(uglify())
-    .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('js/widgets'));
 })
 
 gulp.task('compressi18n', function () {
   gulp.src('../src/main/resources/org/got5/tapestry5/jquery/ui_1.12.1/i18n/*.js')
     .pipe(uglify())
-    .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('js/i18n'));
 })
 
 gulp.task('compresseffects', function () {
   gulp.src('../src/main/resources/org/got5/tapestry5/jquery/ui_1.12.1/effects/*.js')
     .pipe(uglify())
-    .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('js/effects'));
 })
