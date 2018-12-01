bc648f065eda1a13b09fd981f923da8240fd553e
Balázs Palcsó <palcso.balazs@gmail.com>
2018-12-01 17:51:56 +0100

    Use constant in @OnEvent

--- src/test/java/org/got5/tapestry5/jquery/pages/PageScroll.java
+++ src/test/java/org/got5/tapestry5/jquery/pages/PageScroll.java
@@ -2,8 +2,10 @@ package org.got5.tapestry5.jquery.pages;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import org.apache.tapestry5.annotations.OnEvent;
 import org.apache.tapestry5.annotations.Property;
+import org.got5.tapestry5.jquery.JQueryEventConstants;
 
 public class PageScroll {
 
@@ -15,7 +17,7 @@ public class PageScroll {
 
     private static final int PAGE_SIZE = 100;
 
-    @OnEvent("nextPage")
+    @OnEvent(JQueryEventConstants.NEXT_PAGE)
     List<Integer> moreValues(int multiplyBy) throws InterruptedException {
         List<Integer> values = new ArrayList<Integer>();
         for (int i = pageNumber * PAGE_SIZE;
